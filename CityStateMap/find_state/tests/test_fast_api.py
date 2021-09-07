245da0a6663441fff7f642858c9c257816659bd6
stuti-pathak <stuti.pathak@bridgelabz.com>
2021-09-07 17:16:16 +0530

    Refactored: test cases results

--- CityStateMap/find_state/tests/test_fast_api.py
+++ CityStateMap/find_state/tests/test_fast_api.py
@@ -10,17 +10,16 @@ class TestFastApiFunction:
     def test_get_state_name_for_given_city_name(self):
         data = {"city_name": "Pune"}
         response = client.post("/states/", json=data)
-        data=json.loads(response.content)
-        assert response.status_code ==200 and data['state']=="Maharashtra"
+        data = json.loads(response.content)
+        assert response.status_code == 200 and data['state'] == "Maharashtra"
 
     def test_state_name_with_misspelled_city_name(self):
         data = {"city_name": "dlh"}
         response = client.post("/states/", json=data)
-        data=json.loads(response.content)
+        data = json.loads(response.content)
         assert response.status_code == 200 and data['state'] == "Delhi"
 
     def test_state_name_with_empty_city_name(self):
         data = {"city_name": ""}
         response = client.post("/states/", json=data)
         assert response.status_code == 400 and b"give a proper city name" in response.content
-
